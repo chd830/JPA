aedf78449fd8b10e9f87d7ebcb452103f470b6d0
EunChong <chd830@naver.com>
2022-08-08 00:31:54 +0900

    setter대신 생성자를 사용하여 값을 할당

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,22 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
+// Setter대신 생성자를 통해서 값을 할당
+// JPA구현 라이브러리가 리플렉션이나 프록시 같은 기술을 사용해서 객체를 생성할 수 있도록 함
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    // 함부로 new로 생성하지 못하게 선언
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
